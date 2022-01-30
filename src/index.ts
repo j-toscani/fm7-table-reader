f3067c69bbbe00fb8c6bfdbd498bc3ed10fdbfde
Julian Toscani <julian_toscani@gmx.de>
2022-01-30 20:09:38 +0100

    draw newly found positions

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,20 @@ setUpCanvas((ctx, canvas, image) => {
   canvas.height = image.height;
   ctx.drawImage(image, 0, 0, image.width, image.height);
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  const { top, left } = getTableDimensions(imageData);
+  const { top, left, ends } = getTableDimensions(imageData);
 
   ctx.putImageData(imageData, 0, 0);
   ctx.strokeStyle = "blue";
-  ctx.strokeRect(500, top + 1, imageData.width / 2, 10);
+  ctx.lineWidth = 5;
+  ctx.strokeRect(
+    left,
+    top,
+    imageData.width - left,
+    ends[ends.length - 1] - top + 2
+  );
+
+  ends.forEach((end) => {
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, end, image.width, 1);
+  });
 });
