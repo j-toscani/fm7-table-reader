fb4a0a495c968b437f445deed639125a72a12fff
Julian Toscani <julian_toscani@gmx.de>
2022-01-30 22:06:49 +0100

    draw table content to other canvas

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,32 @@ setUpCanvas((ctx, canvas, image) => {
   canvas.height = image.height;
   ctx.drawImage(image, 0, 0, image.width, image.height);
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  const { top, left, rows } = getTableDimensions(imageData);
+  const dimensions = getTableDimensions(imageData);
 
   ctx.putImageData(imageData, 0, 0);
-  ctx.strokeStyle = "blue";
-  ctx.lineWidth = 5;
-  ctx.strokeRect(
+  printTable(canvas, dimensions);
+});
+
+function printTable(
+  canvas: HTMLCanvasElement,
+  dimensions: { top: number; left: number; rows: number[]; end: number }
+) {
+  const { top, left, rows, end } = dimensions;
+  const cutCanvas = document.querySelector<HTMLCanvasElement>("#cut")!;
+  const cutCtx = cutCanvas.getContext("2d")!;
+
+  cutCanvas.width = end;
+  cutCanvas.height = rows[rows.length - 1] - top;
+
+  cutCtx.drawImage(
+    canvas,
     left,
     top,
-    imageData.width - left * 2,
-    rows[rows.length - 1] - top + 2
+    cutCanvas.width,
+    cutCanvas.height,
+    0,
+    0,
+    cutCanvas.width,
+    cutCanvas.height
   );
-
-  rows.forEach((row) => {
-    ctx.fillStyle = "red";
-    ctx.fillRect(left, row, imageData.width - left * 2, 1);
-  });
-});
+}
