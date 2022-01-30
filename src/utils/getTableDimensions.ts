350fed6dd532ef5094344bb42e2cb57292eb8935
Julian Toscani <julian_toscani@gmx.de>
2022-01-30 20:09:10 +0100

    add function to find table rows

--- src/utils/getTableDimensions.ts
+++ src/utils/getTableDimensions.ts
@@ -1,4 +1,3 @@
-import getPixelRows from "./getPixelRows";
 import getTableStartY from "./getTableStartY";
 import toGrayLevels from "./toGrayLevels";
 
@@ -6,27 +5,55 @@ export default function getTableDimensions(imageData: ImageData) {
   const transformed = toGrayLevels(imageData);
 
   const top = getTableStartY(transformed);
-  const rowEnds = getRowEnds(transformed, top);
+  const left = findTableLeftEnd(transformed, top);
+  const ends = findTableRows(imageData, top, left);
 
-  return { top, rowEnds };
+  return { top, left, ends };
 }
 
-function getRowEnds(imageData: ImageData, top: number) {
-  const rows = getPixelRows(imageData);
-  const center = Math.round(rows[0].length / 2);
+function findTableLeftEnd(imageData: ImageData, tableTop: number) {
+  const { width, data } = imageData;
 
-  const colors = new Set();
-  const rowYs = [];
+  // TODO: find first black row instead of assuming it
+  const line = data.slice(
+    (tableTop + 1) * width * 4,
+    (tableTop + 2) * width * 4
+  );
+  const center = Math.floor(line.length / 2);
 
-  for (let index = top; index < rows.length; index++) {
-    const color = rows[index][center];
-    colors.add(color);
-
-    if (color < 40 && color > 30) {
-      rowYs.push(index);
+  let left = 0;
+  for (let index = center; index > 0; index -= 4) {
+    const color = line[index];
+    if (color > 125) {
+      left = index / 4;
       break;
     }
   }
 
-  console.log(colors, rowYs, top);
+  return left;
+}
+
+function findTableRows(imageData: ImageData, top: number, left: number) {
+  const { data, width, height } = imageData;
+  let rowEnds: number[] = [];
+  let count = 0;
+
+  for (let index = top; index < height; index++) {
+    if (count >= 4) {
+      rowEnds = rowEnds.slice(0, -3);
+      break;
+    }
+
+    const pixelIndex = left * 4 + index * width * 4;
+    const pixelColor = data[pixelIndex];
+
+    if (pixelColor < 125) {
+      rowEnds.push(index);
+      count++;
+    } else {
+      count = 0;
+    }
+  }
+
+  return rowEnds;
 }
